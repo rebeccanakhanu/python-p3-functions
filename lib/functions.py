fda56f7a7b4b8ba0dbd93005b4efded6e301800d
Wakhungu N Rebecca <rebeccanakhanu@gmail.com>
2023-05-16 19:35:49 +0300

    completed assignments

--- lib/functions.py
+++ lib/functions.py
@@ -4,13 +4,13 @@ def greet_programmer():
      print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    return (45 + 55)
 
 def halve(number):
-    pass
+    return  number / 2
